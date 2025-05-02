a37ae0ad4d9b1e53cf3d719c692aa7de97cb7995
Prayogi Pangestu <fmother731@gmail.com>
2025-05-02 07:40:10 +0700

    Finish knightTravail function

--- knight-travail.js
+++ knight-travail.js
@@ -34,9 +34,43 @@ function knightMoves(start, end) {
   const queue = [start];
   // Save visited node from queue
   const visitedNode = [];
-  const parent = {};
+  const parentNode = {};
 
   while (queue.length !== 0) {
     // Get current location of knight from queue
+    let [currentX, currentY] = queue.shift();
+    visitedNode.push([currentX, currentY]);
+    // check for all possible knight directions
+    knightDirections.forEach((direction) => {
+      let [nextPosX, nextPosY] = direction;
+      nextPosX += currentX;
+      nextPosY += currentY;
+      // if knight has valid moves(Knight didn't go outside board)
+      // and the node has never been visited
+      if (isValid(nextPosX, nextPosY, visitedNode)) {
+        //  push node to queue and mark node to visited
+        // get reference of parent node to find the shortest path
+        queue.push([nextPosX, nextPosY]);
+        visitedNode.push([nextPosX, nextPosY]);
+        parentNode[`${nextPosX}, ${nextPosY}`] = [currentX, currentY];
+      }
+    });
   }
+  // Find path from the target node to start node
+  const target = end;
+  const path = [];
+  let targetParent = target;
+  // Do looping until targett find path to starting node
+  while (targetParent[0] !== start[0] && targetParent[1] !== start[1]) {
+    path.push(targetParent);
+    // Find parent of the target node
+    targetParent = parentNode[`${targetParent[0]}, ${targetParent[1]}`];
+  }
+  path.push(targetParent);
+  path.reverse();
+  return path;
 }
+
+console.log(knightMoves([0, 0], [3, 3]));
+console.log(knightMoves([3, 3], [0, 0]));
+console.log(knightMoves([0, 0], [7, 7]));
